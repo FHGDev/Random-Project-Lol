c0279b510889ae78ad559ad84a405acc05644bf0
FHGDev <gideon.lingle14@gmail.com>
2017-11-13 12:05:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,5 +5,11 @@ bot.on("ready", () => {
   console.log("Real Life Hulk Simulator v1.0 loaded")
   bot.user.setGame("Hulkbot Dev...")
 })
+ 
+bot.on("message", message => {
+  if (message.content.startsWith("Hey Hulk") {
+     message.reply("Sup")
+   }
+})
 
 bot.login(process.env.token)
